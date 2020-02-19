a3020007b77c82ba377f2afa77ef325a43676892
David Tsaryov <dtsaryov@zuzex.com>
2020-02-19 21:19:29 +0300

    initial

---
+++ run.py
@@ -0,0 +1,11 @@
+from PySide2.QtWidgets import QApplication
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # widget = MyWidget()
+    # widget.resize(800, 600)
+    # widget.show()
+
+    sys.exit(app.exec_())
